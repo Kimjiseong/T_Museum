e31e35e8e3a5ad749140f26cf1422c2bb988b545
kimhue0901 <cox3098@gmail.com>
2017-02-08 21:14:23 +0900

    #1
    초기화

---
+++ Assets/ExplanationClose.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplanationClose : MonoBehaviour
+{
+    //★Explanation Panel의 자식 비활성화
+    public void Close()
+    {
+        GameObject.Find("Explanation Panel").transform.FindChild("Title Panel").gameObject.SetActive(false);
+        GameObject.Find("Explanation Panel").transform.FindChild("Main Panel").gameObject.SetActive(false);
+        GameObject.Find("Explanation Panel").transform.FindChild("Close Button").gameObject.SetActive(false);
+    }
+}
